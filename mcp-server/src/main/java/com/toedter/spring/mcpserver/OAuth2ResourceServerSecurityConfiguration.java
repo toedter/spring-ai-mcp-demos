9320750c302a32bd3d610a5e3cbab78961f55d5c
Kai Toedter <kai@toedter.com>
2025-06-28 10:20:53 +0200

    feat: secure mcp server with OAuth2 and jwt

---
+++ mcp-server/src/main/java/com/toedter/spring/mcpserver/OAuth2ResourceServerSecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.toedter.spring.mcpserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class OAuth2ResourceServerSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET, "/**").hasAuthority("SCOPE_mcp.tools")
+                        .requestMatchers(HttpMethod.POST, "/**").hasAuthority("SCOPE_mcp.tools")
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
+        return http.build();
+    }
+
+}
