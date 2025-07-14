be387bd9a836366e0b0a02f0949c4292f0d51e6e
Kai Toedter <kai@toedter.com>
2025-07-14 19:12:04 +0200

    feat: add mcp client using Ollama Mistral

--- mcp-client/src/main/java/com/toedter/spring/mcpclient/ChatController.java
+++ mcp-client/src/main/java/com/toedter/spring/mcpclient/ChatController.java
@@ -1,8 +1,6 @@
 package com.toedter.spring.mcpclient;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,17 +15,18 @@ public class ChatController {
     public ChatController(ChatClient.Builder chatClientBuilder, ToolCallbackProvider tools) {
 
         this.chatClient = chatClientBuilder
-                .defaultSystem("Please prioritise context information for answering questions")
+                .defaultSystem("Call tools when asked about movies or quotes.")
                 .defaultToolCallbacks(tools)
                 .build();
     }
 
     @GetMapping("/chat")
-    public String chat(@RequestParam String query) {
-        PromptTemplate promptTemplate = new PromptTemplate(query);
-        Prompt prompt = promptTemplate.create();
-        ChatClient.CallResponseSpec res = chatClient.prompt(prompt).call();
-
-        return res.content();
+    public String chat(@RequestParam String prompt) {
+        System.out.println("Chatting with prompt: " + prompt);
+        return this.chatClient
+                .prompt()
+                .user(prompt)
+                .call()
+                .content();
     }
 }
