e13ed8e025b776650986368b4c32cac43eda161b
Kai Toedter <kai@toedter.com>
2025-09-19 08:05:47 +0200

    chore: add authorization token test

--- mcp-server/src/main/java/com/toedter/spring/mcpserver/QuoteService.java
+++ mcp-server/src/main/java/com/toedter/spring/mcpserver/QuoteService.java
@@ -1,33 +1,41 @@
 /*
-* Copyright 2024 - 2024 the original author or authors.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* https://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2024 - 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.toedter.spring.mcpserver;
 
 import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuoteService {
-	@Tool(description = "Get Kai’s favorite movie quote.")
-	public String getKaisFavoriteMovieQuote() {
-		return "With great power comes great responsibility.";
-	}
+    @Tool(description = "Get Kai’s favorite movie quote.")
+    public String getKaisFavoriteMovieQuote() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof JwtAuthenticationToken jwtAuth) {
+            String accessToken = jwtAuth.getToken().getTokenValue();
+            System.out.println("Access token: " + accessToken);
+        }
+        return "With great power comes great responsibility.";
+    }
 
-	public static void main(String[] args) {
-		QuoteService client = new QuoteService();
-		System.out.println(client.getKaisFavoriteMovieQuote());
-	}
+    public static void main(String[] args) {
+        QuoteService client = new QuoteService();
+        System.out.println(client.getKaisFavoriteMovieQuote());
+    }
 
 }
