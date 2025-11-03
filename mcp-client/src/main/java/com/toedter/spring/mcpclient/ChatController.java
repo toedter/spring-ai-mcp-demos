71facdbcee9171cccac1201bb64467ecad092a63
Kai Toedter <kai@toedter.com>
2025-11-03 14:14:36 +0100

    feat: use Siemens LLMs for MCP client demo (Ollama will not run on my notebook)

--- mcp-client/src/main/java/com/toedter/spring/mcpclient/ChatController.java
+++ mcp-client/src/main/java/com/toedter/spring/mcpclient/ChatController.java
@@ -1,6 +1,7 @@
 package com.toedter.spring.mcpclient;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,19 +14,26 @@ public class ChatController {
     private final ChatClient chatClient;
 
     public ChatController(ChatClient.Builder chatClientBuilder, ToolCallbackProvider tools) {
-
         this.chatClient = chatClientBuilder
-                .defaultSystem("You are a friendly assistant.")
+                .defaultSystem("You are a friendly assistant that can use tools when needed.")
                 .defaultToolCallbacks(tools)
                 .build();
     }
 
     @GetMapping("/chat")
-    public String chat(@RequestParam String prompt) {
-        System.out.println("Chatting with prompt: " + prompt);
+    public String chat(@RequestParam(required = false) String message) {
+
+        if (message == null) {
+            return "Please provide 'message' parameter";
+        }
+
+        System.out.println("Chatting with prompt: " + message);
         return this.chatClient
                 .prompt()
-                .user(prompt)
+                .user(message)
+//                .options(OpenAiChatOptions.builder()
+//                        .parallelToolCalls(false)
+//                        .build())
                 .call()
                 .content();
     }
